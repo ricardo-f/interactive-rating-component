05c4fd85e0f560c9fb6312d650bd45efd647127a
ricardo-f <riclima1990@gmail.com>
2023-05-08 22:10:11 -0300

    Remove of ref files and workable solution

--- scripts/script.js
+++ scripts/script.js
@@ -2,40 +2,40 @@ document.addEventListener("DOMContentLoaded", function () {
   const ratingButtons = document.querySelector('.rating-buttons');
   const submitButton = document.querySelector('.submit');
   const ratingCard = document.querySelector('.rating-card');
-  const thanksRating = document.querySelector('.thanks-rating');
+  const thanksCard = document.querySelector('.thanks-card');
   const showEval = document.querySelector('.show-eval');
-  const flashcard = document.querySelector('.flashcard-section');
+  const warnMsgCard = document.querySelector('.warn-msg-card');
+  const closeWrnMsg = document.querySelector('.close-wrn-msg');
   let rating
 
   for (let i = 1; i <= 5; i++) {
     const button = document.createElement('button');
     button.innerText = i;
-    button.classList.add(`button`);
+    button.classList.add(`grades`);
     ratingButtons.appendChild(button);
   }
 
   ratingButtons.addEventListener('click', (event) => {
-    if (event.target.className === 'button') {
+    if (event.target.className === 'grades') {
       rating = parseInt(event.target.innerText);
-      console.log(rating);
       return rating;
     }
   });
 
   submitButton.addEventListener('click', (event) => {
     if (!rating) {
-      console.log(rating)
-      flashcard.style.display = 'block';
+      warnMsgCard.style.display = 'flex';
+      ratingCard.style.display = 'none';
       return;
     }
     ratingCard.style.display = 'none';
-    thanksRating.style.display = 'block';
+    thanksCard.style.display = 'flex';
     let showEvalText = `You selected ${rating} here out of 5`;
     showEval.innerText = showEvalText;
   });
 
-  const flashcardOKButton = document.querySelector('.close-flashcard');
-  flashcardOKButton.addEventListener('click', (event) => {
-    flashcard.style.display = 'none';
+  closeWrnMsg.addEventListener('click', (event) => {
+    warnMsgCard.style.display = 'none';
+    ratingCard.style.display = 'flex';
   });
 });
